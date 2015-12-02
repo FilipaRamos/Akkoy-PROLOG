8ff515e2c4551bd06c3272678254256f528ff74f
FilipaRamos <fimamora@hotmail.com>
2015-12-02 19:38:29 +0000

    finalmente uma soluçãoooooo

--- testInput.pl
+++ testInput.pl
@@ -1,3 +1,11 @@
+:- use_module(library(lists)).
+:- use_module(library(clpfd)).
+
+:- dynamic count/1.
+:- dynamic restList/1.
+
+:- assert(count(0)).
+
 readInput :- read(Command), interpret(Command).
 
 interpret(Command) :- Command = 'play', play.
@@ -8,31 +16,24 @@ replaceP(_, _, [], []).
 replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2).
 replaceP(O, R, [H|T], [H|T2]) :- dif(H,O), replaceP(O, R, T, T2).
 
-pls5(V):-
-        V = [C1,C2,C3,C4,S1,S2,S3,S4],
-        domain(V, 1, 4),
-        %Amarelo = 1, Azul= 2; Preto = 3, Verde = 4,
-        all_different([C1,C2,C3,C4]),
-        all_different([S1,S2,S3,S4]),
-        element(PosAzul, [C1,C2,C3,C4], 2),
-        PosAntesAzul #= PosAzul - 1,
-        element(PosAntesAzul, [S1,S2,S3,S4], SizeAntesAzul),
-        PosDepoisAzul #= PosAzul + 1,
-        element(PosDepoisAzul, [S1,S2,S3,S4], SizeDepoisAzul),
-        SizeAntesAzul #< SizeDepoisAzul,
-        element(PosVerde, [C1,C2,C3,C4], 4),
-        element(PosVerde, [S1,S2,S3,S4], SizeGreen),
-        SizeGreen #= 1,
-        PosVerde #> PosAzul, 
-        element(PosAmarelo, [C1,C2,C3,C4], 1),
-        element(PosPreto, [C1,C2,C3,C4], 3),
-        PosAmarelo #> PosPreto,
-        labeling([], V).
 
+%%%%%%%%%%% PASSAR TABULEIRO LISTA DE LISTAS COM VARIÁVEIS -> 1 - BLACK  2 - WHITE
+%%%%%%%%%%% PASSAR RESTRICOES POR COLUNA E POR LINHA
+
+terminateCount :- assert(count(0)), write(' NOVA CONTAGEM ').
+
+processWhite(Count, [H|T]) :- H = Count, write(' ADICIONADO A LISTA ').
+
+processBlack(Elem, Count, [H|T]) :- NewCount is Count + 1, if(Elem = 1, assert(count(NewCount)), if(Count > 0, processWhite(Count, [H|T]), terminateCount)) .
+
+countConsecutiveBlack(Row, 0, List).
+countConsecutiveBlack(Row, NRow, List) :- NRow > 0, retract(count(Count)), nth1(NRow, Row, Elem), processBlack(Elem, Count, List), write(List), NewN is NRow - 1, countConsecutiveBlack(Row, NewN, List).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-solutions(C) :-
-		%% BLACK IS 1 // WHITE IS 2
-		C = [C1, C2, C3, C4],
-		domain(C, 1, 2).
+%%solutions(B, R) :-
+%%		%% BLACK IS 1 // WHITE IS 2
+%%		B = [A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5],
+%%		domain(B, 1, 2).
+
+

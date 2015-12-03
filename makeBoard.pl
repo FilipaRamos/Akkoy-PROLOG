7e9e7f275029fc02199fa2eb66448216e3c1d76e
FilipaRamos <fimamora@hotmail.com>
2015-12-03 18:00:58 +0000

    codigo praticamente acabado falta testar e por a pinta

--- makeBoard.pl
+++ makeBoard.pl
@@ -30,3 +30,65 @@ createArea(Size, AreaSize, NrAreas, Matrix, NewMatrix) :- random(1, Size, LineN)
 							replace(Line, NewIndex, 2, NewLine), NewL is LineN - 1, replace(Matrix, NewL, NewLine, NewMatrix), findAdjacent(NewMatrix, Size, AreaSize, LineN, Index).
 
 findAdjacent(Matrix, Size, AreaSize, X, Y).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+randomBoard(B) :- 
+		B = [A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4],
+		domain(B, 1, 2),
+		countConsecutiveWhite([A1, A2, A3, A4], List1),
+		length(List1, Size1),
+		Size1 #<= 2,
+		countConsecutiveWhite([B1, B2, B3, B4], List2),
+		length(List2, Size2),
+		Size2 #= Size1,
+		countConsecutiveWhite([C1, C2, C3, C4], List3),
+		length(List3, Size3),
+		Size3 #= Size1,
+		countConsecutiveWhite([D1, D2, D3, D4], List4),
+		length(List4, Size4),
+		Size4 #= Size1,
+		labeling([ffc], B).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+getRestrictions(Board, TransposedBoard, WhiteRest, BlackRest, 0).
+getRestrictions(Board, TransposedBoard, WhiteRest, BlackRest, Size) :- Size > 0, nth1(Size, Board, Row), length(Row, RowLength), nth1(Size, TransposedBoard, Column), length(Column, ColumnLength),
+						countConsecutiveWhite(Row, RowLength, 0, ResultWhite), append(WhiteRest, ResultWhite, WhiteRest), 
+						countConsecutiveBlack(Column, ColumnLength, 0, ResultBlack), append(BlackRest, ResultBlack, BlackRest),
+						NewSize is Size - 1, getRestrictions(Board, WhiteRest, NewSize). 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% DISPLAY A BOARD ELEMENT %%
+
+displayBoardElem(Index, NrLine, Board) :- nth0(NrLine, Board, Line), nth0(Index, Line, Elem), if(Elem = 1, write(' * '), write('   ')).
+
+%% DISPLAY HORIZONTAL LINES %%
+
+countLines(Size, Lines) :- Dash is Size - 1, HalfLine is 3*Size, Lines is Dash + HalfLine.
+
+displayLine(Size) :- countLines(Size, Lines), write(' '), writeLine(Lines), nl.
+
+writeLine(0).
+writeLine(Lines) :- Lines > 0, write('-'), NewNr is Lines - 1, writeLine(NewNr).
+
+%% DISPLAY VERTICAL LINES %%
+
+writeDashes(Size, _NrLine, Size, Board).
+writeDashes(Size, _NrLine, Index, Board) :- Index < Size, write('|'), displayBoardElem(Index, _NrLine, Board), 
+			NewIndex is Index + 1, writeDashes(Size, _NrLine, NewIndex, Board).
+
+createList(0,[]).
+createList(Size, [H|T]) :- Size > 0, NewSize is Size - 1, H = '   ', 
+					createList(NewSize, T).
+
+createMatrix(0, [], _MatLength).
+createMatrix(Size, [H|T], _MatLength) :- Size > 0, NewSize is Size - 1, createList(_MatLength, List),
+					H = List, createMatrix(NewSize, T, _MatLength).
+
+%% DISPLAY BOARD %%
+
+displayRandomBoard(Size, Size, _Res, Board) :- displayLine(Size).
+displayRandomBoard(Size, NrLine, _Res, Board) :- displayLine(Size), writeDashes(Size, NrLine, 0, Board), 
+							write('|'), nl,  NewLine is NrLine + 1, displayRandomBoard(Size, NewLine, _Res, Board).
--- testInput.pl
+++ testInput.pl
@@ -1,5 +1,6 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
+:- use_module(library(samsort)).
 
 :- dynamic count/1.
 :- dynamic restList/1.
@@ -31,12 +32,90 @@ countConsecutiveBlack(Row, NRow, Count, Result) :- NRow > 0, nth1(NRow, Row, Ele
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%solutions(B, RC, RL) :-
+processRest(0, Column, List).
+processRest(Size, Column, List) :- Size > 0,  nth1(Size, Column, ElemCol), nth1(Size, List, ElemList), 
+						ElemCol #= ElemList, 
+						NewSize is Size - 1, processRest(NewSize, Column, List).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%solutions(B, RWhite, RBlack) :-
 %%		%% BLACK IS 1 // WHITE IS 2
 %%		B = [A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5, D1, D2, D3, D4, D5, E1, E2, E3, E4, E5],
-%%		RC = [RC1, RC2, RC3, RC4, RC5],
-%%		RL = [RL1, RL2, RL3, RL4, RL5],
-%% 		retract(count(C)),
-%%		countConsecutiveBlack([A1, A2, A3, A4, A5], 5, C),
-%%		domain(B, 1, 2).
+%%		domain(B, 1, 2),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%      			RESTRINGIR COLUNAS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([A1, A2, A3, A4, A5], 5, List1),
+%%		length(List1, Size1),
+%%		nth1(1, RBlack, Column1), length(Column1, Length1),
+%%		Length1 #= Size1,
+%%		samsort(Column1, NewColumn1), samsort(List1, NewList1), 
+%%		processRest(Size1, NewColumn1, NewList1),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([B1, B2, B3, B4, B5], 5, List2),
+%%		length(List2, Size2),
+%%		nth1(2, RBlack, Column2), length(Column2, Length2),
+%%		Length2 #= Size2,
+%%		samsort(Column2, NewColumn2), samsort(List2, NewList2), 
+%%		processRest(Size2, NewColumn2, NewList2),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([C1, C2, C3, C4, C5], 5, List3),
+%%		length(List3, Size3),
+%%		nth1(3, RBlack, Column3), length(Column3, Length3),
+%%		Length3 #= Size3,
+%%		samsort(Column3, NewColumn3), samsort(List3, NewList3), 
+%%		processRest(Size3, NewColumn3, NewList3),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([D1, D2, D3, D4, D5], 5, List4),
+%%		length(List4, Size4),
+%%		nth1(4, RBlack, Column4), length(Column4, Length4),
+%%		Length4 #= Size4,
+%%		samsort(Column4, NewColumn4), samsort(List4, NewList4), 
+%%		processRest(Size4, NewColumn4, NewList4),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([E1, E2, E3, E4, E5], 5, List5),
+%%		length(List5, Size5),
+%%		nth1(5, RBlack, Column5), length(Column5, Length5),
+%%		Length5 #= Size5,
+%%		samsort(Column5, NewColumn5), samsort(List5, NewList5), 
+%%		processRest(Size5, NewColumn5, NewList5),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%					RESTRINGIR LINHAS						
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([A1, A2, A3, A4, A5], 5, List1),
+%%		length(List1, Size1),
+%%		nth1(1, RBlack, Column1), length(Column1, Length1),
+%%		Length1 #= Size1,
+%%		samsort(Column1, NewColumn1), samsort(List1, NewList1), 
+%%		processRest(Size1, NewColumn1, NewList1),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([B1, B2, B3, B4, B5], 5, List2),
+%%		length(List2, Size2),
+%%		nth1(2, RBlack, Column2), length(Column2, Length2),
+%%		Length2 #= Size2,
+%%		samsort(Column2, NewColumn2), samsort(List2, NewList2), 
+%%		processRest(Size2, NewColumn2, NewList2),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([C1, C2, C3, C4, C5], 5, List3),
+%%		length(List3, Size3),
+%%		nth1(3, RBlack, Column3), length(Column3, Length3),
+%%		Length3 #= Size3,
+%%		samsort(Column3, NewColumn3), samsort(List3, NewList3), 
+%%		processRest(Size3, NewColumn3, NewList3),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([D1, D2, D3, D4, D5], 5, List4),
+%%		length(List4, Size4),
+%%		nth1(4, RBlack, Column4), length(Column4, Length4),
+%%		Length4 #= Size4,
+%%		samsort(Column4, NewColumn4), samsort(List4, NewList4), 
+%%		processRest(Size4, NewColumn4, NewList4),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		countConsecutiveBlack([E1, E2, E3, E4, E5], 5, List5),
+%%		length(List5, Size5),
+%%		nth1(5, RBlack, Column5), length(Column5, Length5),
+%%		Length5 #= Size5,
+%%		samsort(Column5, NewColumn5), samsort(List5, NewList5), 
+%%		processRest(Size5, NewColumn5, NewList5),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%		labeling([ffc], B).
 

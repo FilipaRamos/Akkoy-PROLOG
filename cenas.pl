24f8e2163fa1c6a709db0fa23308743d4ad926b4
FilipaRamos <fimamora@hotmail.com>
2015-12-11 03:11:40 +0000

    que bug é este?

--- cenas.pl
+++ cenas.pl
@@ -71,16 +71,39 @@ getPossibilities(S, Begins, R1):-
 formLines([Rfirst], [f(Var,Rfirst,1)], [Var], Rfirst) :- !.
 formLines([Rfirst|Rs], [f(Var,Rfirst,1)|Lines], [Var|Begins], LastVal) :- formLines(Rs, Lines, Begins, LastVal).
 
-restrict(Begins, Length, Length, Row).
-restrict(Begins, Length, Counter, Row) :- 
+restrictWhite(Begins, Length, Length, Row).
+restrictWhite(Begins, Length, Counter, Row) :- 
 		Counter < Length, 
 		nth0(Counter, Begins, Index),
-		element(Index, Row, Elem),
+		nth0(Index, Row, Elem),
 		Elem #= 0,
 		NewCounter is Counter + 1,
-		restrict(Begins, Length, NewCounter, Row).
+		restrictWhite(Begins, Length, NewCounter, Row).
 
-processBegins(Begins, Row) :- length(Begins, Length), restrict(Begins, Length, Counter, Row).
+processBeginsWhiteAux(Begins, Row, Length, Length).
+processBeginsWhiteAux(Begins, Row, Length, Counter) :- Counter < Length, 
+						nth0(Counter, Begins, OneBegin),
+						length(OneBegin, BeginLength),
+						restrictWhite(OneBegin, BeginLength, 0, Row),
+						NewCounter is Counter + 1,
+						processBeginsWhiteAux(Begins, Row, Length, NewCounter).
+
+processBeginsWhite(Begins, Row) :- length(Begins, Length), processBeginsWhiteAux(Begins, Row, Length, 0).
+
+restrictBlack(Begins, Length, Length, Row).
+restrictBlack(Begins, Length, Counter, Row) :- 
+		Counter < Length, 
+		nth0(Counter, Begins, Index),
+		nth0(Index, Row, Elem),
+		Elem #= 1,
+		NewCounter is Counter + 1,
+		restrictBlack(Begins, Length, NewCounter, Row).
+
+processBeginsBlackAux(Begins, Row, Length, Length).
+processBeginsBlackAux(Begins, Row, Length, Counter) :- Counter < Length, nth0(Counter, Begins, OneBegin), length(OneBegin, BeginLength), restrictBlack(OneBegin, BeginLength, 0, Row),
+						NewCounter is Counter + 1, processBeginsBlackAux(Begins, Row, Length, NewCounter).  
+
+processBeginsBlack(Begins, Row) :- length(Begins, Length), processBeginsBlackAux(Begins, Row, Length, 0).
 
 findBegins(AllBegins, Rest) :- findall(Begins, getPossibilities(4, Begins, Rest), AllBegins).
 
@@ -91,49 +114,58 @@ teste :-
 	countWhite(Vars,L),
 	transpose(Vars, TransposedVars),
 	countBlack(TransposedVars, Black),
+
+	nth1(1, L, Rest1),
+	nth1(2, L, Rest2),
+	nth1(3, L, Rest3),
+	nth1(4, L, Rest4),
+
+	findBegins(Begins1, Rest1),
+	findBegins(Begins2, Rest2),
+	findBegins(Begins3, Rest3),
+	findBegins(Begins4, Rest4),
+
+	%%%%%%%%%%%%%%%%%
+
+	nth1(1, Black, Black1),
+	nth1(2, Black, Black2),
+	nth1(3, Black, Black3),
+	nth1(4, Black, Black4),
+
+	findBegins(Bg1, Black1),
+	findBegins(Bg2, Black2),
+	findBegins(Bg3, Black3),
+	findBegins(Bg4, Black4),
+
 	Vars2 = [[E1,E2,E3,E4], [F1,F2,F3,F4], [G1,G2,G3,G4], [H1,H2,H3,H4]],
 	domain([F1,F2,F3,F4], 0, 1),
 	domain([E1,E2,E3,E4], 0, 1),
 	domain([G1,G2,G3,G4], 0, 1),
 	domain([H1,H2,H3,H4], 0, 1),
 
+	processBeginsWhite(Begins1, [E1,E2,E3,E4]),
 
-	nth1(1, L, Rest1),
-	findBegins(Begins1, Rest1),
-	processBegins(Begins1, [E1,E2,E3,E4]),
+	processBeginsWhite(Begins2, [F1,F2,F3,F4]),
 
-	nth1(2, L, Rest2),
-	findBegins(Begins2, Rest2),
-	processBegins(Begins2, [F1,F2,F3,F4]),
+	processBeginsWhite(Begins3, [G1,G2,G3,G4]),
 
-	nth1(3, L, Rest3),
-	findBegins(Begins3, Rest3),
-	processBegins(Begins3, [G1,G2,G3,G4]),
-
-	nth1(4, L, Rest4),
-	findBegins(Begins4, Rest4),
-	processBegins(Begins4, [H1,H2,H3,H4]),
+	processBeginsWhite(Begins4, [H1,H2,H3,H4]),
 
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-	nth1(1, Black, Black1),
-	findBegins(Bg1, Black1),
-	processBegins(Bg1, [E1,F1,G1,H1]),
+	write('onde esta a merdar?'),nl, nl, 
 
-	nth1(2, Black, Black2),
-	findBegins(Bg2, Black2),
-	processBegins(Bg2, [E2,F2,G2,H2]),
+	processBeginsBlack(Bg1, [E1,F1,G1,H1]),
 
-	nth1(3, Black, Black3),
-	findBegins(Bg3, Black3),
-	processBegins(Bg3, [E3,F3,G3,H3]),
+	processBeginsBlack(Bg2, [E2,F2,G2,H2]),
 
-	nth1(4, Black, Black4),
-	findBegins(Bg4, Black4),
-	processBegins(Bg4, [E4,F4,G4,H4]),
+	processBeginsBlack(Bg3, [E3,F3,G3,H3]),
 
-	labeling([],[F1,F2,F3,F4]),
+	processBeginsBlack(Bg4, [E4,F4,G4,H4]),
+
+	write('labeling?'),nl, nl, 
 	labeling([],[E1,E2,E3,E4]),
+	labeling([],[F1,F2,F3,F4]),
 	labeling([],[G1,G2,G3,G4]),
 	labeling([],[H1,H2,H3,H4]),
 
